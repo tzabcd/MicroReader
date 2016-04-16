88b97ff35d9a43b59b6beb9ec1c4177e1f87a462
‘caiyao’ <whucaiyao@gmail.com>
2016-04-16 11:16:59 +0800

    优化启动界面

--- app/src/main/java/name/caiyao/microreader/ui/activity/WelcomeActivity.java
+++ app/src/main/java/name/caiyao/microreader/ui/activity/WelcomeActivity.java
@@ -28,9 +28,10 @@ import java.util.Random;
 import name.caiyao.microreader.R;
 import name.caiyao.microreader.api.zhihu.ZhihuRequest;
 import name.caiyao.microreader.bean.image.ImageResponse;
-import name.caiyao.microreader.config.Config;
 import name.caiyao.microreader.utils.SharePreferenceUtil;
 import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class WelcomeActivity extends BaseActivity {
@@ -51,7 +52,7 @@ public class WelcomeActivity extends BaseActivity {
         DateFormat dateFormat = SimpleDateFormat.getDateInstance(SimpleDateFormat.DATE_FIELD);
         date = dateFormat.format(new Date());
 
-        if (!sharedPreferences.getString(SharePreferenceUtil.IMAGE_GET_TIME, "").equals(date)) {
+        if (!sharedPreferences.getString(SharePreferenceUtil.IMAGE_GET_TIME, "").equals(date) && SharePreferenceUtil.isChangeThemeAuto(this)) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 new AlertDialog.Builder(this).setMessage(getString(R.string.request_storage_permission)).setPositiveButton("知道了", new DialogInterface.OnClickListener() {
                     @Override
@@ -96,67 +97,61 @@ public class WelcomeActivity extends BaseActivity {
     }
 
     private void getBackground() {
-        if (SharePreferenceUtil.isChangeThemeAuto(this)) {
-            ZhihuRequest.getZhihuApi().getImage().subscribeOn(Schedulers.io())
-                    .subscribe(new Observer<ImageResponse>() {
-                        @Override
-                        public void onCompleted() {
-
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            e.printStackTrace();
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Toast.makeText(WelcomeActivity.this, getString(R.string.common_loading_error), Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-                                    finish();
-                                }
-                            });
-                        }
-
-                        @Override
-                        public void onNext(ImageResponse imageReponse) {
-                            if (imageReponse.getData() != null && imageReponse.getData().getImages() != null) {
-                                try {
-                                    Bitmap bitmap = BitmapFactory.decodeStream(new URL("http://wpstatic.zuimeia.com/" + imageReponse.getData().getImages().get(0).getImageUrl() + "?imageMogr/v2/auto-orient/thumbnail/480x320/quality/100").openConnection().getInputStream());
-                                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, new FileOutputStream(new File(getFilesDir().getPath() + "/bg.jpg")));
-                                    Palette palette = Palette.from(bitmap).generate();
-                                    int color = 0x000000;
-                                    int vibrant = palette.getVibrantColor(color);
-                                    int vibrantDark = palette.getDarkVibrantColor(color);
-                                    if (vibrant == 0)
-                                        vibrant = vibrantDark;
-                                    if (vibrant == 0)
-                                        vibrant = getRandomPrimaryColor();
-                                    int muted = palette.getMutedColor(color);
-                                    int mutedDark = palette.getDarkMutedColor(color);
-                                    if (muted == 0)
-                                        muted = mutedDark;
-                                    if (muted == 0)
-                                        muted = ContextCompat.getColor(WelcomeActivity.this,R.color.colorAccent);
-                                    sharedPreferences.edit()
-                                            .putString(SharePreferenceUtil.IMAGE_DESCRIPTION, imageReponse.getData().getImages().get(0).getDescription())
-                                            .putInt(SharePreferenceUtil.VIBRANT, vibrant)
-                                            .putInt(SharePreferenceUtil.MUTED, muted)
-                                            .putString(SharePreferenceUtil.IMAGE_GET_TIME, date)
-                                            .apply();
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
+        ZhihuRequest.getZhihuApi().getImage().subscribeOn(Schedulers.io())
+                .map(new Func1<ImageResponse, Boolean>() {
+                    @Override
+                    public Boolean call(ImageResponse imageResponse) {
+                        if (imageResponse.getData() != null && imageResponse.getData().getImages() != null) {
+                            try {
+                                Bitmap bitmap = BitmapFactory.decodeStream(new URL("http://wpstatic.zuimeia.com/" + imageResponse.getData().getImages().get(0).getImageUrl() + "?imageMogr/v2/auto-orient/thumbnail/480x320/quality/100").openConnection().getInputStream());
+                                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, new FileOutputStream(new File(getFilesDir().getPath() + "/bg.jpg")));
+                                Palette palette = Palette.from(bitmap).generate();
+                                int color = 0x000000;
+                                int vibrant = palette.getVibrantColor(color);
+                                int vibrantDark = palette.getDarkVibrantColor(color);
+                                if (vibrant == 0)
+                                    vibrant = vibrantDark;
+                                if (vibrant == 0)
+                                    vibrant = getRandomPrimaryColor();
+                                int muted = palette.getMutedColor(color);
+                                int mutedDark = palette.getDarkMutedColor(color);
+                                if (muted == 0)
+                                    muted = mutedDark;
+                                if (muted == 0)
+                                    muted = ContextCompat.getColor(WelcomeActivity.this, R.color.colorAccent);
+                                sharedPreferences.edit()
+                                        .putString(SharePreferenceUtil.IMAGE_DESCRIPTION, imageResponse.getData().getImages().get(0).getDescription())
+                                        .putInt(SharePreferenceUtil.VIBRANT, vibrant)
+                                        .putInt(SharePreferenceUtil.MUTED, muted)
+                                        .putString(SharePreferenceUtil.IMAGE_GET_TIME, date)
+                                        .apply();
+                            } catch (IOException e) {
+                                e.printStackTrace();
                             }
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
-                                    finish();
-                                }
-                            });
                         }
-                    });
-        }
+                        return true;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Boolean>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.makeText(WelcomeActivity.this, getString(R.string.common_loading_error), Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                        finish();
+                    }
+
+                    @Override
+                    public void onNext(Boolean imageReponse) {
+                        startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                        finish();
+                    }
+                });
     }
 
     @Override
@@ -181,6 +176,6 @@ public class WelcomeActivity extends BaseActivity {
                 R.color.colorPinkPrimary,
                 R.color.colorPrimary
         };
-        return ContextCompat.getColor(this,primaryInt[new Random().nextInt(14)]);
+        return ContextCompat.getColor(this, primaryInt[new Random().nextInt(14)]);
     }
 }
